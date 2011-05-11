ac47ad4bdaf6d5c76238d4d739bee104bdb17159
commi <me@till.seifert.name>
2011-05-11 14:08:19 +0200

    changes for framework path's

--- config.php
+++ config.php
@@ -16,14 +16,14 @@ $config = array(
 	// Mode 0 hides all error messages
 	// Mode 1 displays error messages related to the style sheets (like elements trying to inherit properties that don't exist)
 	// Mode 2 additionally displays php developer messages and sets error_reporting to E_ALL
-	'debug_level' => 2,
+	'debug_level' => 1,
 
 
 	// Base path to cssp and css files relative to css.php
-	'css_base_dir' => '',
+	'css_base_dir' => '../css',
 	
 	// where to place cache files
-	'cache_dir' => 'cache',
+	'cache_dir' => '../cache/turbine',
 
 	// Minify regular css files (true) oder include them completely unchanged (false)
 	'minify_css' => true,
