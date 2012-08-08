682ca638fbdce998b6f6740a03f6c1ce1697eb82
commi <me@till.seifert.name>
2012-08-08 12:27:37 +0200

    bugfixes for ie only for ie

--- plugins/bugfixes.php
+++ plugins/bugfixes.php
@@ -31,29 +31,40 @@ function bugfixes(&$parsed){
 	$changed['textarea']['overflow'][] = 'auto';
 
 	// IE6: Background image flickers on hover
-	$changed['html']['filter'][] = 'expression(document.execCommand("BackgroundImageCache",false,true))';
+	if($browser->browser == 'ie' && floatval($browser->browser_version) < 7)
+	{
+		$changed['html']['filter'][] = 'expression(document.execCommand("BackgroundImageCache",false,true))';
+	}
 
 	// IE6: Fix transparent PNGs, see http://www.twinhelix.com/css/iepngfix/
-	$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/iepngfix.htc';
-	$changed['img']['behavior'][] = 'url("'.$htc_path.'")';
+	if($browser->browser == 'ie' && floatval($browser->browser_version) < 7)
+	{
+		$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/iepngfix.htc';
+		$changed['img']['behavior'][] = 'url("'.$htc_path.'")';
+	}
 
 	// IE6 and 7: resample images bicubic instead of using nearest neighbor method
 	if($browser->browser == 'ie' && floatval($browser->browser_version) < 7){
 		// IE6: Image margin bottom bug
 		$changed['img']['vertical-align'][] = 'bottom';
-	
+
 		// IE6: Background image flickers on hover
 		$changed['html']['filter'][] = 'expression(document.execCommand("BackgroundImageCache",false,true))';
-	
+
 		// IE6: Fix transparent PNGs, see http://www.twinhelix.com/css/iepngfix/
 		$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/iepngfix.htc';
 		$changed['img']['behavior'][] = 'url("'.$htc_path.'")';
-		
+
 		// IE6: Input align, see http://tjkdesign.com/ez-css/css/base.css
 		$changed['img']['vertical-align'][] = 'text-bottom';
-	}	
-	// IE 7: resample images bicubic instead of using nearest neighbor method
-	$changed['img']['-ms-interpolation-mode'][] = 'bicubic';
+	}
+
+
+	if($browser->browser == 'ie' && floatval($browser->browser_version) < 8)
+	{
+		// IE 7: resample images bicubic instead of using nearest neighbor method
+		$changed['img']['-ms-interpolation-mode'][] = 'bicubic';
+	}
 
 	// IE6 and 7: Enable full styleability for buttons, see http://www.sitepoint.com/forums/showthread.php?t=547059
 	$changed['button']['overflow'][] = 'visible';
@@ -61,11 +72,14 @@ function bugfixes(&$parsed){
 	$changed['button']['white-space'][] = 'nowrap';
 
 	// IE6 and 7: Missing :hover-property on every tag except a, see http://www.xs4all.nl/~peterned/csshover.html
-	$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/csshover3.htc';
-	$changed['body']['behavior'][] = 'url("'.$htc_path.'")';
-	// IE8: Reenable cleartype where filters are set
-	$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/';
-	$changed['body']['behavior'][] = 'url("'.$htc_path.'csshover3.htc") url("'.$htc_path.'cleartypefix.htc")';
+	if($browser->browser == 'ie' && floatval($browser->browser_version) < 8)
+	{
+		$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/csshover3.htc';
+		$changed['body']['behavior'][] = 'url("'.$htc_path.'")';
+		// IE8: Reenable cleartype where filters are set
+		$htc_path = rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/plugins/bugfixes/';
+		$changed['body']['behavior'][] = 'url("'.$htc_path.'csshover3.htc") url("'.$htc_path.'cleartypefix.htc")';
+	}
 
 	// Firefox: Ghost margin around buttons, see http://www.sitepoint.com/forums/showthread.php?t=547059
 	$changed['button::-moz-focus-inner']['padding'][] = '0';
@@ -73,7 +87,7 @@ function bugfixes(&$parsed){
 
 	// Webkit: better antialiasing, see http://maxvoltar.com/archive/-webkit-font-smoothing
 	$changed['html']['-webkit-font-smoothing'][] = 'antialiased';
-	
+
 	// Webkit: better kerning, see http://www.aestheticallyloyal.com/public/optimize-legibility/
 	$changed['html']['text-rendering'][] = 'optimizeLegibility';
 
